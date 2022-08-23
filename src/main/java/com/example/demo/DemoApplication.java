44ea297f77feaceba4a6676a9b3e9a8405fba85c
Prashant Gupta <prashant.gupta@tothenew.com>
2022-08-23 18:10:58 +0530

    Initial Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,10 +11,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		Sentry.init(options -> {
 			options.setDsn("https://eb15b6c47320401780d376e2524f49b8@o319267.ingest.sentry.io/1864663");
-			// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-			// We recommend adjusting this value in production.
 			options.setTracesSampleRate(1.0);
-			// When first trying Sentry it's good to see what the SDK is doing:
 			options.setDebug(true);
 		});
 	}
